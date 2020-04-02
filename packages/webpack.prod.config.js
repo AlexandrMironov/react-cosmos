6f8240fca1cac4054e07ee55223a00a8b709827c
alexandr <alex.mironov.1996@gmail.com>
2020-04-02 14:37:53 +0300

    v1.0.1

--- packages/webpack.prod.config.js
+++ packages/webpack.prod.config.js
@@ -56,6 +56,6 @@ module.exports = {
   externals: {
     'react': 'React',
     'react-dom': 'ReactDOM',
-    'reduxjs/toolkit': 'reduxjs/toolkit'
+    'reduxjs/toolkit': 'reduxjs/toolkit',
   },
 };
